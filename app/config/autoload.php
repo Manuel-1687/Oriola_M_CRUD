3d253d669de2ca0505a8987f8dee0dbd8247e6a8
Manuel-1687 <oriolapardz@gmail.com>
2026-09-16 16:22:45 +0800

    Load URL helper for login form

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -82,7 +82,7 @@ $autoload['libraries'] = array('session');
 |
 |	$autoload['helpers'] = array('url', 'file');
 */
-$autoload['helpers'] = array();
+$autoload['helpers'] = array('url');
 
 /*
 | -------------------------------------------------------------------
